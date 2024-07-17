2f813d53c88b0ac6902ea4d13bf68bae17711c29
NikolayAfanasev <bigniko@mail.ru>
2024-07-17 15:50:19 +0300

    VendingMachine's class constructor and addProduct method added

--- VendingMachine/src/VendingMachines/VendingMachine.java
+++ VendingMachine/src/VendingMachines/VendingMachine.java
@@ -1,4 +1,22 @@
 package VendingMachines;
 
+import Products.Product;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class VendingMachine {
+    private int volume;
+    private List<Product> products;
+    private List<String> workLog;
+
+    public  VendingMachine(int volume){
+        this.volume = volume;
+        products = new ArrayList<Product>();
+        workLog = new ArrayList<String>();
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
+    }
 }
