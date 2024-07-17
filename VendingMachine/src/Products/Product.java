83b4e0ba56e7fed56656dd6351ee0d5eaf60e0dc
NikolayAfanasev <bigniko@mail.ru>
2024-07-17 13:30:34 +0300

    save progress

--- VendingMachine/src/Products/Product.java
+++ VendingMachine/src/Products/Product.java
@@ -1,4 +1,14 @@
 package Products;
 
+import java.security.PublicKey;
+
 public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price){
+        this.name = name;
+        this.price = price;
+    }
+
 }
